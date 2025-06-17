587d349d1cb9a58c68c7be358c5db6ab2a4d31a4
Honore Amounan <78807934+HonoreBallar@users.noreply.github.com>
2025-06-17 09:30:31 +0000

    update logo, some image

--- script.js
+++ script.js
@@ -15,3 +15,37 @@ document.getElementById("backToTop").addEventListener("click", function () {
         behavior: "smooth"
     });
 });
+
+const element = document.getElementById('typewriter');
+const skills = element.dataset.skills.split(',');
+
+let skillIndex = 0;
+let charIndex = 0;
+let typing = true;
+
+function typeEffect() {
+  const currentSkill = skills[skillIndex];
+  
+  if (typing) {
+    charIndex++;
+    if (charIndex <= currentSkill.length) {
+      element.textContent = currentSkill.substring(0, charIndex);
+    } else {
+      typing = false;
+      setTimeout(typeEffect, 1000); // pause après écriture
+      return;
+    }
+  } else {
+    charIndex--;
+    if (charIndex >= 0) {
+      element.textContent = currentSkill.substring(0, charIndex);
+    } else {
+      typing = true;
+      skillIndex = (skillIndex + 1) % skills.length;
+    }
+  }
+  
+  setTimeout(typeEffect, typing ? 150 : 100); // vitesse d'écriture / suppression
+}
+
+typeEffect();
